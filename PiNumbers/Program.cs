e084d20c04a4dacdccc356b90f9f136d02d2a10a
nawsish <nawsish2018@gmail.com>
2025-02-25 11:34:05 +0600

    add two test

---
+++ PiNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+        double R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double pi = 3.14159;
+        double A = pi * R * R;
+
+        Console.WriteLine($"A={A:F4}");
+    }
+
+}
---
+++ SumNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the input values for A and B
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        // Calculate the sum of A and B
+        int X = A + B;
+
+        // Print the result in the required format
+        Console.WriteLine($"X = {X}");
+    }
+}
