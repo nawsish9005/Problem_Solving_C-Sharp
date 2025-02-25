938ee1a364fc8cbc4ce03cd4d173145f84fb749d
nawsish <nawsish2018@gmail.com>
2025-02-25 14:03:59 +0600

    add average numbers

---
+++ AverageExample2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 10.0;
+
+        Console.WriteLine($"MEDIA = {MEDIA:F1}");
+    }
+}
