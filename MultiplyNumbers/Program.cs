a4592abbf8dba106c2e0aa04df7e8e1dd8585267
nawsish <nawsish2018@gmail.com>
2025-02-25 12:18:10 +0600

    add problem solving

---
+++ MultiplyNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int PROD = A * B;
+
+        Console.WriteLine($"PROD = {PROD}");
+    }
+}
