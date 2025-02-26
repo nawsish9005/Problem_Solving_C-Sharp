c49cb9594387922e08456e0b420bb1462816c4dd
nawsish <nawsish2018@gmail.com>
2025-02-26 17:50:25 +0600

    upload kml

---
+++ FindKml/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+
+//Calculate a car's average consumption being provided the total 
+//distance traveled (in Km) and the spent fuel total (in liters).
+class Program
+{
+    static void Main()
+    {
+        // Read integer value X (total distance)
+        int X = int.Parse(Console.ReadLine());
+
+        // Read floating point value Y (total fuel spent)
+        double Y = double.Parse(Console.ReadLine());
+
+        // Calculate average consumption
+        double result = X / Y;
+
+        // Print result with 3 decimal places
+        Console.WriteLine($"{result:F3} km/l");
+    }
+}
---
+++ greatestValue/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Make a program that reads 3 integer values and present the greatest one followed by the message 
+// "eh o maior". Use the following formula:
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+       string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        int C = int.Parse(input[2]);
+        
+        int result = (A + B+Math.Abs(A-B))/2;
+        int maior = (result + C + Math.Abs(result - C)) / 2;
+        
+        Console.WriteLine($"{maior} eh o maior");
+
+    }
+
+}
