76df8cb4bca813ea620122de8d71476b43ae0182
nawsish <nawsish2018@gmail.com>
2025-02-26 17:36:59 +0600

    add comment

--- AreaCalculation/Program.cs
+++ AreaCalculation/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System; 
 
+// Make a program that reads three floating point values: A, B and C. Then, calculate and show:
+// a) the area of the rectangled triangle that has base A and height C.
+// b) the area of the radius's circle C. (pi = 3.14159)
+// c) the area of the trapezium which has A and B by base, and C by height.
+// d) the area of ​​the square that has side B.
+// e) the area of the rectangle that has sides A and B.
 class URI {
 
     static void Main(string[] args) { 
