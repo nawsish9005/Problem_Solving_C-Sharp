6637da63d6011238aa0a2b791fe44a31f134a4c1
nawsish <nawsish2018@gmail.com>
2025-02-26 11:14:59 +0600

    add salary calculation

---
+++ findOutSalary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int number = int.Parse(Console.ReadLine());
+        int workHour = int.Parse(Console.ReadLine());
+        double hourlyRate = double.Parse(Console.ReadLine());
+        
+        double salary = workHour * hourlyRate;
+        Console.WriteLine($"NUMBER = {number}");
+        Console.WriteLine($"SALARY = U$ {salary:F2}");
+
+    }
+
+}
