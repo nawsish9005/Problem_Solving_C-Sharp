cff1529850f84454975ffa57cb0f850e3415268d
nawsish <nawsish2018@gmail.com>
2025-02-26 13:14:01 +0600

    add salarywithbonus

---
+++ salaryWithBonus/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string userName=Console.ReadLine();
+        double fixedSalary=double.Parse(Console.ReadLine());
+        double totalSale=double.Parse(Console.ReadLine());
+        
+        double totalSalary=fixedSalary+(totalSale * 0.15);
+        
+        Console.WriteLine($"TOTAL = {totalSalary:F2}");
+
+    }
+
+}
