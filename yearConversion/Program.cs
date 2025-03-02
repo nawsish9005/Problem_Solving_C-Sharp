e9907c6e76730d80eb9a06ef45eb4be239b87d99
nawsish <nawsish2018@gmail.com>
2025-03-02 11:14:58 +0600

    add year calculation

---
+++ yearConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int days = int.Parse(Console.ReadLine());
+        
+        int years = days / 365;
+         days = days % 365;
+        int months = days/30;
+        
+        days = days % 30;
+
+        Console.WriteLine($"{years}: {days}: {months}");
+    }
+
+}
