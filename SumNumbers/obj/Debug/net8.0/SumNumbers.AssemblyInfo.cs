e714669be7243cbf6a462c9d8fd95ee4799be221
nawsish <nawsish2018@gmail.com>
2025-03-09 09:39:50 +0600

    add snacks

--- SumNumbers/obj/Debug/net8.0/SumNumbers.AssemblyInfo.cs
+++ SumNumbers/obj/Debug/net8.0/SumNumbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SumNumbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a67a01486d451ab0cd02a9f1a6d6846ddea8b16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa5d6512dcdf2ed85b3032c946fc6241f03ebe6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SumNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SumNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ snacks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) {
+
+        Dictionary<int, double> priceTable = new Dictionary<int, double>
+        {
+            { 1, 4.00 },
+            { 2, 4.50 },
+            { 3, 5.00 },
+            { 4, 2.00 },
+            { 5, 1.50 }
+        };
+
+        string[] input = Console.ReadLine().Split(' ');
+        int x = int.Parse(input[0]);
+        int y = int.Parse(input[1]);
+
+        double total = priceTable[x] * y;
+        Console.WriteLine($"Total: R$ {total:F2}");
+    }
+
+}
